0b6b7175e9a83724e32f99d4cda3c53585a7f102
azkalonz <aeartixenter@gmail.com>
2020-09-22 17:04:43 +0800

    minor fix

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::post("/test",function(){
+Route::get("/test",function(){
     return ["test"=>123];
 });
 Route::middleware('auth:api')->get('/user', function (Request $request) {

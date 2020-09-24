019e6519324e373d2da876e260f8459051d64136
azkalonz <aeartixenter@gmail.com>
2020-09-24 11:50:30 +0800

    test

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Hook;
 /*
 |--------------------------------------------------------------------------
-| API Routes2
+| API Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
